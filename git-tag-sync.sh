eb88b0e53ac5de0904c72385b7a0837d5da928ba
Will Ho <ikaruswill.ho@gmail.com>
2020-05-15 21:06:23 +0800

    Refactor code bash script into functions

--- git-tag-sync.sh
+++ git-tag-sync.sh
@@ -5,74 +5,91 @@
 # REPO
 # UPSTREAM
 # SSH_KEY
-echo "Configuring SSH..."
+
+# SSH variables
 KNOWN_HOSTS_FILE='./known_hosts'
 SSH_PRIVATE_KEY_FILE='./id_rsa'
-
 SSH_PATH=$HOME/.ssh/
-mkdir -p $SSH_PATH
-cp $KNOWN_HOSTS_FILE $SSH_PATH
-cp $SSH_PRIVATE_KEY_FILE $SSH_PATH
 
+# Repository variables
 REPO_URL='https://github.com/ikaruswill/gitea.git'
 UPSTREAM_URL='https://github.com/go-gitea/gitea.git'
 REPO_ROOT='/repos'
 
+configure_ssh () {
+    mkdir -p $SSH_PATH
+    cp $KNOWN_HOSTS_FILE $SSH_PATH
+    cp $SSH_PRIVATE_KEY_FILE $SSH_PATH
+}
+
+check_repo_url () {
+    REPO_HTTPS_URL=$(echo $REPO_URL | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p')
+    if [ -z "$REPO_HTTPS_URL" ]; then
+        echo "Repo URL is using SSH"
+    else
+        echo "Repo URL is using HTTPS, converting to SSH..."
+        USER=$(echo $REPO_HTTPS_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\1#p')
+        if [ -z "$USER" ]; then
+            echo "-- ERROR:  Could not identify User."
+            exit
+        fi
+
+        REPO=$(echo $REPO_HTTPS_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\2#p')
+        if [ -z "$REPO" ]; then
+            echo "-- ERROR:  Could not identify Repo."
+            exit
+        fi
+
+        NEW_URL="git@github.com:$USER/$REPO.git"
+        echo "Changing repo url from "
+        echo "  '$REPO_HTTPS_URL'"
+        echo "      to "
+        echo "  '$NEW_URL'"
+        echo ""
+
+        REPO_URL=$NEW_URL
+    fi
+}
+
+pull_or_clone_repo () {
+    if [ -d $REPO_PATH ]; then
+        echo "Local repo exists"
+        echo "Pulling repository..."
+        git -C $REPO_PATH pull origin
+    else 
+        echo "Local repo not cloned yet"
+        echo "Cloning repository..."
+        git clone $REPO_URL $REPO_PATH
+        git -C $REPO_PATH remote add upstream $UPSTREAM_URL
+    fi
+}
+
+push_tags () {
+    if [ -z "$TAGS" ]; then
+        echo "Origin up-to-date with upstream"
+    else 
+        echo "Origin behind upstream"
+        for tag in $TAGS; do
+            echo "Pushing $tag..."
+            git -C $REPO_PATH push origin $tag
+        done
+    fi
+}
+
+echo "Configuring SSH..."
+configure_ssh
+
 echo "Checking repo URL..."
-REPO_HTTPS_URL=`echo $REPO_URL | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p'`
-if [ -z "$REPO_HTTPS_URL" ]; then
-    echo "Repo URL is using SSH"
-else
-    echo "Repo URL is using HTTPS, converting to SSH..."
-    USER=`echo $REPO_HTTPS_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\1#p'`
-    if [ -z "$USER" ]; then
-        echo "-- ERROR:  Could not identify User."
-        exit
-    fi
+check_repo_url
 
-    REPO=`echo $REPO_HTTPS_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\2#p'`
-    if [ -z "$REPO" ]; then
-        echo "-- ERROR:  Could not identify Repo."
-        exit
-    fi
-
-    NEW_URL="git@github.com:$USER/$REPO.git"
-    echo "Changing repo url from "
-    echo "  '$REPO_HTTPS_URL'"
-    echo "      to "
-    echo "  '$NEW_URL'"
-    echo ""
-
-    REPO_URL=$NEW_URL
-fi
-
+echo "Pull or clone repository..."
 REPO=$(echo $REPO_URL | sed -n 's/^.*\/\(.*\)\.git$/\1/p')
 REPO_PATH=$REPO_ROOT/$REPO
-
-if [ -d $REPO_PATH ]; then
-    echo "Local repo exists"
-    echo "Pulling repository..."
-    cd $REPO_PATH
-    git pull origin
-else 
-    echo "Local repo not cloned yet"
-    echo "Cloning repository..."
-    git clone $REPO_URL $REPO_PATH
-    cd $REPO_PATH
-    git remote add upstream $UPSTREAM_URL
-fi
+pull_or_clone_repo
 
 echo "Fetching tags..."
 TAGS=$(git fetch upstream --tags 2>&1 | sed -n 's/^.*\[new tag\].*->\s*\(.*\).*$/\1/p')
 
-if [ -z "$TAGS" ]; then
-    echo "Origin up-to-date with upstream"
-else 
-    echo "Origin behind upstream"
-    for tag in $TAGS; do
-        echo "Pushing $tag..."
-        git push origin $tag
-    done
-fi
+push_tags
 
 echo "Done"
