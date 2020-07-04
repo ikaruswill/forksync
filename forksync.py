218a8f49f224c72fb9f3aef1763dbd332f02f60e
Will Ho <ikaruswill.ho@gmail.com>
2020-07-04 22:00:09 +0800

    Add logger, more logging points, and APP_NAME

--- forksync.py
+++ forksync.py
@@ -7,7 +7,8 @@ import urllib
 import confuse
 import git
 
-template = {
+APP_NAME = 'forksync'
+CONFIG_TEMPLATE = {
     'ssh_key': confuse.Filename(),
     'cache_dir': confuse.Filename(),
     'repositories': confuse.Sequence(
@@ -53,49 +54,58 @@ def parse_repo(url):
 
 
 def run_repo(ssh_key, cache_dir, repo_config):
-    repo_config['origin'] = validate_url(repo_config['origin'])
-    repo_config['upstream'] = validate_url(repo_config['upstream'])
     org, repo = parse_repo(repo_config['origin'])
+    repo_id = os.path.join(org, repo)
     repo_path = os.path.join(cache_dir, repo)
 
+    logger.info(f'Processing repository: {repo_id}')
+
+    # Check cache
     try:
         repo = git.Repo(repo_path)
+        logger.info('Repository cache hit')
     except git.exc.NoSuchPathError:
+        logger.info('Repository cache miss, cloning...')
         repo = git.Repo.clone_from(repo_config['origin'], repo_path)
     except git.exc.InvalidGitRepositoryError:
-        logger.warn('Invalid repository, reinitializing...')
+        logger.warn('Cached repository invalid, reinitializing...')
         shutil.rmtree(repo_path)
         repo = git.Repo.clone_from(repo_config['origin'], repo_path)
 
+    # Check origin
     try:
         origin = repo.remote('origin')
     except ValueError:
-        logger.warn('Origin missing from repository, reinitializing...')
+        logger.warn('Origin missing from cached repository, reinitializing...')
         shutil.rmtree(repo_path)
         repo = git.Repo.clone_from(repo_config['origin'], repo_path)
         origin = repo.remote('origin')
 
+    # Check upstream
     try:
         upstream = repo.remote('upstream')
     except ValueError:
         logger.warn('Upstream missing, adding upstream...')
         upstream = repo.create_remote('upstream', repo_config['upstream'])
 
+    # Sync origin
     logger.info('Fetching latest state from origin')
     origin_fetch = origin.fetch(tags=True, prune=True, prune_tags=True)
-
     current_tags = repo.tags
+
+    # Sync upstream
     upstream_fetch = upstream.fetch(tags=True)
     new_tags = repo.tags
 
+    # Check upstream against cache
     missing_tags = list(set(new_tags) - set(current_tags))
     if len(missing_tags):
         logger.info('Origin is behind upstream')
         tag_push_order = sorted(
             missing_tags, key=lambda t: t.commit.committed_datetime)
 
-        # Push each tag individually, Github has webhook limit of 3 tags
-        # per push
+        # Push each tag individually
+        # Github has webhook limit of 3 tags per push
         origin_push = []
         for tag_ref in tag_push_order:
             logger.info(f'Pushing {tag_ref.name}...')
@@ -103,17 +113,21 @@ def run_repo(ssh_key, cache_dir, repo_config):
                 origin.push(f'{tag_ref.path}:{tag_ref.path}')[0])
     else:
         logger.info('Origin is up-to-date with upstream')
+
     logger.info('Done')
 
 
 def run(ssh_key, cache_dir, repositories):
     for repo_config in repositories:
+        repo_config['origin'] = validate_url(repo_config['origin'])
+        repo_config['upstream'] = validate_url(repo_config['upstream'])
         run_repo(ssh_key, cache_dir, repo_config)
+    logger.info('forksync complete')
 
 
 def main():
     parser = argparse.ArgumentParser(
-        prog='forksync',
+        prog=APP_NAME,
         description='Keep a fork synchronized with its upstream')
     parser.add_argument(
         '--ssh-key',
@@ -123,15 +137,15 @@ def main():
         help='Directory to cache repositories in')
 
     args = parser.parse_args()
-    config = confuse.LazyConfig('forksync', __name__)
+    config = confuse.LazyConfig(APP_NAME, __name__)
     config.set_args(args)
-    valid = config.get(template)
+    valid = config.get(CONFIG_TEMPLATE)
     run(**valid)
 
 
 if __name__ == "__main__":
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger()
+    logger = logging.getLogger(APP_NAME)
     main()
